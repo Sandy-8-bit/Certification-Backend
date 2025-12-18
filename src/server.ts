8f07f137faf328122c6c812f4a7d57533c86dde8
sabarish <sabarish_7@protonmail.com>
2025-12-18 15:17:07 +0530

    cors

--- src/server.ts
+++ src/server.ts
@@ -10,14 +10,15 @@ import { globalErrorHandler } from "./middleware/errorHandler";
 import cors from "cors";
 const app: Application = express();
 
+app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: true,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
