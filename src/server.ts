1185642d2936bc30b29d41fc928e011acd748784
sabarish <sabarish_7@protonmail.com>
2025-12-18 15:23:13 +0530

    cors updat

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
